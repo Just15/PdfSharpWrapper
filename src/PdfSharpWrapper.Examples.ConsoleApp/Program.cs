2fb5b058a2e335ecdcd5641fe215575158d2a5dd
Just15 <Just15@users.noreply.github.com>
2021-09-17 20:50:00 -0600

    PdfSharpWrapper.Examples.ConsoleApp

---
+++ src/PdfSharpWrapper.Examples.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace PdfSharpWrapper.Examples.ConsoleApp
+{
+    internal class Program
+    {
+        private readonly PdfDocumentReader pdfDocumentReader;
+        private readonly PdfDocumentWriter pdfDocumentWriter;
+
+        private static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Services.GetRequiredService<Program>().Run();
+        }
+
+        public Program(PdfDocumentReader pdfDocumentReader, PdfDocumentWriter pdfDocumentWriter)
+        {
+            this.pdfDocumentReader = pdfDocumentReader;
+            this.pdfDocumentWriter = pdfDocumentWriter;
+        }
+
+        private void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddScoped<Program>();
+                    services.AddTransient<PdfDocumentReader>();
+                    services.AddTransient<PdfDocumentWriter>();
+                });
+        }
+    }
+}
